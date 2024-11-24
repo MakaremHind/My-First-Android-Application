e59f4dc8ac58eea32b1d672098df969c1dd1a0b3
Hind <hind.a.makarem@gmail.com>
2024-11-24 20:40:28 +0100

    Add NoRoom composable to display message when no room is found

--- app/src/main/java/com/automacorp/RoomActivity.kt
+++ app/src/main/java/com/automacorp/RoomActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,30 +17,46 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.automacorp.service.RoomService
 import com.automacorp.ui.theme.AutomacorpTheme
 
 class RoomActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         val param = intent.getStringExtra(MainActivity.ROOM_PARAM)
+        val room = RoomService.findByNameOrId(param)
 
-        enableEdgeToEdge()
         setContent {
             AutomacorpTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RoomDetail(
-                        name = param ?: "",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    if (room != null) {
+                        RoomDetail(room.toString(), Modifier.padding(innerPadding))
+                    } else {
+                        NoRoom(Modifier.padding(innerPadding))
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun NoRoom(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(R.string.act_room_none),
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
 
