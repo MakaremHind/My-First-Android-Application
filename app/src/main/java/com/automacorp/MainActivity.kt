4506a9ad5184fc6662daeda5c7ae3311bda357fa
Hind <hind.a.makarem@gmail.com>
2024-11-24 14:27:34 +0100

    Update Greeting composable to use localized string for welcome message

--- app/src/main/java/com/automacorp/MainActivity.kt
+++ app/src/main/java/com/automacorp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.automacorp.ui.theme.AutomacorpTheme
 
@@ -32,14 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    Text(
-        text = "I learn to create a new app",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = stringResource(R.string.act_main_welcome),
+            modifier = modifier,
+            textAlign = TextAlign.Center // Optional: Center-align the text
+        )
+        Text(
+            text = "Hello $name!", // Optional: Keep or modify this
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
